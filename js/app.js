b094b653e5e39307fdb3e15c83f213e170331876
robert8597 <66479327+robert8597@users.noreply.github.com>
2021-11-11 16:28:18 +0100

    update

--- js/app.js
+++ js/app.js
@@ -46,6 +46,27 @@ if ("serviceWorker" in navigator) {
     });
   }
   */
+
+  var target = document.getElementById('target');
+var watchId;
+
+function appendLocation(location, verb) {
+  verb = verb || 'updated';
+  var newLocation = document.createElement('p');
+  newLocation.innerHTML = 'Location ' + verb + ': ' + location.coords.latitude + ', ' + location.coords.longitude + '';
+  target.appendChild(newLocation);
+}
+
+if ('geolocation' in navigator) {
+  document.getElementById('askButton').addEventListener('click', function () {
+    navigator.geolocation.getCurrentPosition(function (location) {
+      appendLocation(location, 'fetched');
+    });
+    watchId = navigator.geolocation.watchPosition(appendLocation);
+  });
+} else {
+  target.innerText = 'Geolocation API not supported.';}
+  
   function takePhoto() {
     if (!('ImageCapture' in window)) {
       alert('ImageCapture is not available');
