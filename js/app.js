40967a21438942ab564b15ed7a257c54a4303dcd
robert8597 <66479327+robert8597@users.noreply.github.com>
2021-12-12 15:51:39 +0100

    sw

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ const container = document.querySelector(".container");
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", function() {
     navigator.serviceWorker
-      .register("/serviceWorker.js")
+      .register("/sw.js")
       .then(res => console.log("service worker registered"))
       .catch(err => console.log("service worker not registered", err));
   });
