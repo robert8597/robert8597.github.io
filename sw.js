1eacc3e5f492f42526caf927d6a2bff2734abe27
robert8597 <66479327+robert8597@users.noreply.github.com>
2021-12-13 19:09:35 +0100

    v1

--- sw.js
+++ sw.js
@@ -1,17 +1,51 @@
 
+const serviceworker_version = 1.0
+
+
+const staticDevCoffee = "dev-coffee-site-v1"
+const assets = [
+    "/",
+    "/index.html",
+    "/css/style.css",
+    "/js/app.js",
+    "/tabelle.png"
+]
+
+self.addEventListener("install", installEvent => {
+    installEvent.waitUntil(
+        caches.open(staticDevCoffee).then(cache => {
+            cache.addAll(assets)
+        })
+    )
+})
+
+self.addEventListener("fetch", fetchEvent => {
+    fetchEvent.respondWith(
+        caches.match(fetchEvent.request).then(res => {
+            return res || fetch(fetchEvent.request)
+        })
+    )
+})
+
+
+
+
 
 const expectedCaches = ['testCustomers'];
 
 self.addEventListener('install', event => {
-  console.log('V2 installing…');
-
+  console.log('V2 installing…9');
+  console.log("Service worker version="+serviceworker_version);
   // cache a horse SVG into a new cache, static-v2
-  event.waitUntil(
-    caches.open('static-v2').then(cache => cache.add('/horse.svg'))
-  );
+  
+});
+
+self.addEventListener('message', function(event) {
+  event.ports[0].postMessage({'test': 'This is my response.'});
 });
 
 self.addEventListener('activate', event => {
+  console.log("Service worker version="+serviceworker_version);
   // delete any caches that aren't in expectedCaches
   // which will get rid of static-v1
   event.waitUntil(
